eb27b88779334bd45f55b91b7893f8296b6c983b
@slawomir.slowik <slawomirslowik@yahoo.pl>
2018-12-30 18:22:53 +0100

    coś namieszałem z branchami

--- Automaton/Program.cs
+++ Automaton/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
 using System.Linq;
 using Automaton.feature_ex_2;
+using Automaton.feature_ex_3;
 
 namespace Automaton
 {
@@ -8,17 +12,49 @@ namespace Automaton
     {
         static void Main(string[] args)
         {
+
+            // ex 2 - zabawa z interfejsami
             HelloSpeaker speak = new HelloSpeaker();
             ////speak.SayHello(ELanguage.PL);
-            //Console.ReadKey();
-
+            
             ELanguage[] languages = (ELanguage[])Enum.GetValues(typeof(ELanguage));
 
-            foreach (ELanguage lang in languages)
+            foreach (var lang in languages)
             {
                 speak.SayHello(lang);
             }
-                Console.ReadKey(); 
+
+            // ex 3 - zabawa z plikami
+            // generuję po 5 losowych plików z rozszerzeniem .txt, .zip, .xml korzystajać z utworzonej klasy FileGenerator:  
+
+            string filesPath = @"D:\Random\";
+
+            FileGenerator filon = new FileGenerator();
+            for (var i = 0; i < 5; i++)
+            {
+                filon.WriteFile(filon.GenerateRandomFileName(filesPath, "txt"));
+
+                filon.WriteFile(filon.GenerateRandomFileName(filesPath, "zip"));
+
+                filon.WriteFile(filon.GenerateRandomFileName(filesPath, "xml"));
+            }
+
+            var fileList = Directory.GetFiles(filesPath);
+
+            for (var i = 0; i < fileList.Length; i++)
+            {
+                Console.WriteLine("File No " + (i + 1) + ": " + fileList[i]);
+            }
+
+            var querry1 = fileList.Select(n => n).ToArray();
+
+            //usuwam pliki z rozszerzeniem .xml używając LINQ dla DirectoryInfo
+
+            DirectoryInfo di = new DirectoryInfo(filesPath);
+            var files = di.GetFiles();
+            files.AsParallel().Where(f => (f.Extension == ".xml" || f.Extension == ".zip")).ForAll((f) => f.Delete());
+
+            Console.ReadKey(); 
         }
     }
 }
