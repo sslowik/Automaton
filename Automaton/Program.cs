bf0df2d4c86ce1003ada0483573bd3ec802add4c
@slawomir.slowik <slawomirslowik@yahoo.pl>
2018-12-30 17:34:57 +0100

    generator losowych plików

--- Automaton/Program.cs
+++ Automaton/Program.cs
@@ -12,19 +12,28 @@ namespace Automaton
     {
         static void Main(string[] args)
         {
+
+            // ex 2 - zabawa z interfejsami
             HelloSpeaker speak = new HelloSpeaker();
             ////speak.SayHello(ELanguage.PL);
             
             ELanguage[] languages = (ELanguage[])Enum.GetValues(typeof(ELanguage));
 
-            foreach (ELanguage lang in languages)
+            foreach (var lang in languages)
             {
                 speak.SayHello(lang);
             }
 
+            // ex 3 - zabawa z plikami
+            // generuję po 5 losowych plików z rozszerzeniem .txt, .zip, .xml: 
+
             FileGenerator filon = new FileGenerator();
-            filon.WriteRandomFilePath();  
-            Console.ReadKey();
+            var theFile = filon.GenerateRandomFileName(@"D:\C\", "txt");
+
+            filon.WriteFile(theFile); 
+
+           
+
         }
     }
 }
--- Automaton/feature-ex-3/FileGenerator.cs
+++ Automaton/feature-ex-3/FileGenerator.cs
@@ -9,27 +9,58 @@ namespace Automaton.feature_ex_3
 {
     public class FileGenerator
     {
-        public void WriteRandomFilePath()
+        public string GenerateRandomFileName(string filePath, string fileExtension)
         {
+            if (!System.IO.Directory.Exists(filePath))
+            {
+                System.IO.Directory.CreateDirectory(filePath);
+            }
 
-            string filePath = string.Format(@"d:\temp\{0}.txt", Path.GetRandomFileName());
-            Console.WriteLine(filePath);
-
+            string randomFile = (filePath + string.Concat(Path.GetRandomFileName().Replace(".","") + "." + fileExtension));
+            return randomFile;
         }
-//        for (var i = 0; i< 5; i++)
-//            {
 
-//                using (FileStream fs = File.Create(filePath))
-//                {
-//                    FileGenerator.(fs, "Some text");
-//                }
+        public void WriteFile(string randomFile)
+        {
+            if (!System.IO.File.Exists(randomFile))
+            {
+                using (System.IO.FileStream fs = System.IO.File.Create(randomFile))
+                {
+                    for (byte i = 0; i < 100; i++)
+                    {
+                        fs.WriteByte(i);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("File \"{0}\" already exists.", randomFile);
+                return;
+            }
+        }
 
-//            }
 
-//        private static void AddText(FileStream fs, string value)
-//        {
-//            byte[] info = new UTF8Encoding(true).GetBytes(value);
-//            fs.Write(info, 0, info.Length);
-//        }
+        public static string GenerateFileName(string extension = "")
+        {
+            return string.Concat(Path.GetRandomFileName().Replace(".", ""),
+                (!string.IsNullOrEmpty(extension)) ? (extension.StartsWith(".") ? extension : string.Concat(".", extension)) : "");
+        }
+
+        
+        //        for (var i = 0; i< 5; i++)
+        //            {
+
+        //                using (FileStream fs = File.Create(filePath))
+        //                {
+        //                    FileGenerator.(fs, "Some text");
+        //                }
+
+        //            }
+
+        //        private static void AddText(FileStream fs, string value)
+        //        {
+        //            byte[] info = new UTF8Encoding(true).GetBytes(value);
+        //            fs.Write(info, 0, info.Length);
+        //        }
     }
 }
