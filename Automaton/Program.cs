8bb685288f78b81740fdaa929c0974a97a40edb8
@slawomir.slowik <slawomirslowik@yahoo.pl>
2018-12-30 18:18:19 +0100

    usuwanie plików o określonym rozszerzeniu

--- Automaton/Program.cs
+++ Automaton/Program.cs
@@ -25,15 +25,36 @@ namespace Automaton
             }
 
             // ex 3 - zabawa z plikami
-            // generuję po 5 losowych plików z rozszerzeniem .txt, .zip, .xml: 
+            // generuję po 5 losowych plików z rozszerzeniem .txt, .zip, .xml korzystajać z utworzonej klasy FileGenerator:  
+
+            string filesPath = @"D:\Random\";
 
             FileGenerator filon = new FileGenerator();
-            var theFile = filon.GenerateRandomFileName(@"D:\C\", "txt");
+            for (var i = 0; i < 5; i++)
+            {
+                filon.WriteFile(filon.GenerateRandomFileName(filesPath, "txt"));
 
-            filon.WriteFile(theFile); 
+                filon.WriteFile(filon.GenerateRandomFileName(filesPath, "zip"));
 
-           
+                filon.WriteFile(filon.GenerateRandomFileName(filesPath, "xml"));
+            }
 
+            var fileList = Directory.GetFiles(filesPath);
+
+            for (var i = 0; i < fileList.Length; i++)
+            {
+                Console.WriteLine("File No " + (i + 1) + ": " + fileList[i]);
+            }
+
+            var querry1 = fileList.Select(n => n).ToArray();
+
+            //usuwam pliki z rozszerzeniem .xml używając LINQ dla DirectoryInfo
+
+            DirectoryInfo di = new DirectoryInfo(filesPath);
+            var files = di.GetFiles();
+            files.AsParallel().Where(f => f.Extension == ".xml").ForAll((f) => f.Delete());
+
+            Console.ReadKey(); 
         }
     }
 }
